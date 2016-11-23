a0503c91a3735059b99013ee149526b2747e6ebe
Millhouse <uazori@gmail.com>
2016-11-23 12:36:53 +0200

    fix  getById bug

--- chess-rating-server/src/main/java/com/millhouse/chessrating/dao/PlayerDaoImp.java
+++ chess-rating-server/src/main/java/com/millhouse/chessrating/dao/PlayerDaoImp.java
@@ -23,39 +23,38 @@ public class PlayerDaoImp implements PlayerDao {
 
         Query query = sessionFactory.getCurrentSession().createQuery("from Player where id =:playerId");
         query.setParameter("playerId", id);
-        List<?> playerList = query.list();
-        if (playerList.size() == 0){throw new RuntimeException("Player with id "+id+" not found");}
-        return (Player) playerList.get(0);
+        List<Player> playerList = query.getResultList();
+        if (playerList == null || playerList.isEmpty()) {
+            return null;
+        }
+        return playerList.get(0);
 
     }
 
     @Override
     public Player getByName(String name) {
 
-        Query query = sessionFactory.getCurrentSession().createQuery("from Player where name =:playerName");
+        Query query = sessionFactory.getCurrentSession().createQuery("from Player where name = :playerName");
         query.setParameter("playerName", name);
-        List<?> playerList = query.list();
-        if (playerList.size() == 0) {
-            return null;
+        return (Player) query.getSingleResult();
+
+    }
+
+    @Override
+    public void saveOrUpdate(Player player) {
+        if (player.getId() == null) {
+            sessionFactory.getCurrentSession().save(player);
+        } else {
+            sessionFactory.getCurrentSession().saveOrUpdate(player);
         }
-        return (Player) playerList.get(0);
-    }
-
-    @Override
-    public void savePlayer(Player player) {
-        sessionFactory.getCurrentSession().persist(player);
-    }
-
-    @Override
-    public void updatePlayer(Player player) {
-        sessionFactory.getCurrentSession().saveOrUpdate(player);
     }
 
     @Override
     public void deletePlayerById(Long id) {
-    Query query = sessionFactory.getCurrentSession().createQuery("delete Player where id =:playerId");
+        Query query = sessionFactory.getCurrentSession().createQuery("delete Player where id =:playerId");
         query.setParameter("playerId", id);
-        int result = query.executeUpdate();
+        query.executeUpdate();
+        sessionFactory.getCurrentSession().clear();
     }
 
     @Override
@@ -66,13 +65,5 @@ public class PlayerDaoImp implements PlayerDao {
         return query.list();
     }
 
-    @Override
-    public void deleteAllPlayers() {
 
-    }
-
-    @Override
-    public boolean isPlayerExist(Player player) {
-        return false;
-    }
 }
