004d790d522066f7a3b223e224ac2afa5aef65eb
Millhouse <uazori@gmail.com>
2016-11-23 12:49:05 +0200

    Test configuration add

---
+++ chess-rating-server/src/test/java/com/millhouse/chessrating/configuration/TestDaoConfig.java
@@ -0,0 +1,12 @@
+package com.millhouse.chessrating.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Millhouse on 11/22/2016.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.millhouse.chessrating")
+public class TestDaoConfig {
+}
